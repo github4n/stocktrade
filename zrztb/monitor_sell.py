111561d86bf86f873b0f2cc58a5fd668ab9f55aa
tuoxie119 <516024151@qq.com>
2016-06-03 16:39:21 +0800

    增加最大值操作

--- zrztb/monitor_sell.py
+++ zrztb/monitor_sell.py
@@ -12,9 +12,8 @@ import easytrader as et
 conn = pymongo.MongoClient('192.168.222.188', port=27017)
 user = et.use('xq')
 user.prepare('xq.json')
-
-flag = 1
-while flag:
+today = time.strftime("%Y-%m-%d",time.localtime())
+while 1:
     if(time.strftime("%H:%M:%S", time.localtime())< '09:30:00'):
         continue
 
@@ -28,31 +27,40 @@ while flag:
 
         try:
             df = ts.get_realtime_quotes(item['code'])
+
             #取得开始时间开始的最大值
             starttime = item['buytime'].split(' ')[0]
-            # df1 = ts.get_h_data(item['code'],start=starttime)['high']
-            # maxprice = df1.max()
-            maxprice = round(item['maxprice'],2)
-            nowprice = df['price'][0]
-            profit = round((float(nowprice) - float(item['buyprice'])) / float(item['buyprice']) * 100,2)
+            if starttime == today:
+                continue
 
+            #最大值需要替换前面程序
+            maxprice = round(item['maxprice'],2)
+            nowprice = df['price']
+            #更新最大收益价格值
+            if nowprice > maxprice:
+                conn.mystock.trade.update({'code': item['code']},{'$set': {'maxprice': nowprice}})
+
+            #当前收益
+            profit = round((float(nowprice) - float(item['buyprice'])) / float(item['buyprice']) * 100,2)
+            #最大收益
             maxprofit = round((float(maxprice) - float(item['buyprice'])) / float(item['buyprice']) * 100,2)
 
+            sellprice = float(nowprice) * 0.8
             #止损点为5个点
             if profit < -5:
                 print 'sell stock:', item['code']
-                user.sell(item['code'], nowprice * 0.8, item['count'])
-                conn.mystock.trade.update({'code': item['code']}, {'$set': {'tradestatus': 1, 'sellprice': nowprice,'selldate': time.strftime("%Y-%m-%d",time.localtime())}})
+                conn.mystock.trade.update({'code': item['code']}, {'$set': {'tradestatus': 1, 'sellprice': nowprice,'selldate': today}})
+                user.sell(item['code'], price=sellprice, amount=item['stockcount'])
 
             #止盈点为最大收益回落5个点
             if maxprofit -5 >= profit:
                 print 'sell stock:',item['code']
-                user.sell(item['code'],nowprice*0.8,item['count'])
-                conn.mystock.trade.update({'code': item['code']}, {'$set': {'tradestatus': 1,'sellprice':nowprice,'selldate':time.strftime("%Y-%m-%d", time.localtime())}})
+                conn.mystock.trade.update({'code': item['code']}, {'$set': {'tradestatus': 1,'sellprice':nowprice,'selldate':today}})
+                user.sell(item['code'], price=sellprice, amount=item['stockcount'])
 
         except Exception as e:
             print e
             continue
 
-        flag = 0
+
 
