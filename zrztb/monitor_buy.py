4e416fe3cf6708bf956af3ff41522ed7fee763a1
tuoxie119 <516024151@qq.com>
2016-06-13 11:18:32 +0800

    修改买入价格和买入数量

--- zrztb/monitor_buy.py
+++ zrztb/monitor_buy.py
@@ -31,7 +31,7 @@ while 1:
     for item in conn.mystock.monitor_weakhardencode.find({"isdeal":0}):
         try:
             df = ts.get_realtime_quotes(item['code'])
-            conn.mystock.monitor_his.insert(json.loads(df.to_json(orient='records')))
+            # conn.mystock.monitor_his.insert(json.loads(df.to_json(orient='records')))
 
             if(item['status']=='init'):
                 #判断低开
@@ -51,12 +51,15 @@ while 1:
                     print "买入价格", df['price'][0]
                     conn.mystock.monitor_weakhardencode.update({'code': item['code'],'status':'lowopen'},{'$set': {'status': 'lowopencross0','isdeal':1}})
 
-                    buyprice = round(float(df['price'][0]*1.02),2)
-                    stockcount = int(user.balance[0]['enable_balance'] / 3 / (buyprice * 100))
+                    buyprice = round(float(df['price'][0])*1.02,2)
+                    stockcount = int(user.balance[0]['enable_balance'] / (5-len(user.position)) / (buyprice))
                     # stockcount = int(922281 / 3 / (buyprice * 100))
                     conn.mystock.trade.insert({"code":item['code'],"buytime":time.strftime("%Y-%m-%d %X", time.localtime()),"buytype":"zrztb","detailtype":"lowopencross0","buyprice":df['price'][0],"tradestatus":0,'stockcount':stockcount})
                     user.buy(item['code'],float(df['price'][0]),stockcount)
                     print '账户买入成功'
+                    print '剩余可用资金', user.balance[0]['enable_balance']
+                    print '买入数量', stockcount
+
             #高开低走
             if (item['status'] == 'highopen'):
                 if (df['price'] < df['open']).bool():
@@ -71,9 +74,10 @@ while 1:
                     conn.mystock.monitor_weakhardencode.update({'code': item['code'],'status':'highopenlow'},{'$set': {'status': 'highopenlowhigh','isdeal':1}})
 
                     #买入价格高于当前价格，以确定肯定能买到
-                    buyprice = round(float(df['price'][0] * 1.02), 2)
+                    buyprice = round(float(df['price'][0]) * 1.02, 2)
                     #购买股票数量
-                    stockcount = int(user.balance[0]['enable_balance'] / 5 / (buyprice * 100))
+                    stockcount = int(user.balance[0]['enable_balance'] / (5-len(user.position)) / (buyprice))
+
 
                     if stockcount<= 0:
                         print '资金不足'
@@ -85,6 +89,8 @@ while 1:
                          "tradestatus": 0, 'stockcount': stockcount})
                     user.buy(item['code'], float(df['price'][0]), stockcount)
                     print '账户买入成功'
+                    print '剩余可用资金', user.balance[0]['enable_balance']
+                    print '买入数量', stockcount
 
 
         except Exception as e:
