b0d86c3e4551ba88454e24c963fd764613633f60
tuoxie119 <516024151@qq.com>
2016-06-15 10:50:17 +0800

    持仓数量小于策略数量时，才购买

--- zrztb/monitor_buy.py
+++ zrztb/monitor_buy.py
@@ -12,6 +12,7 @@ import easytrader as et
 user = et.use('xq')
 user.prepare('xq.json')
 
+maxstockcount = 6
 
 conn = pymongo.MongoClient('192.168.222.188', port=27017)
 inserttime = time.strftime("%Y-%m-%d", time.localtime())
@@ -49,16 +50,19 @@ while 1:
                     print "D0，推荐买入！！！",item['code']
                     print "买入时间：", time.strftime("%Y-%m-%d %X", time.localtime())
                     print "买入价格", df['price'][0]
-                    conn.mystock.monitor_weakhardencode.update({'code': item['code'],'status':'lowopen'},{'$set': {'status': 'lowopencross0','isdeal':1}})
 
-                    buyprice = round(float(df['price'][0])*1.02,2)
-                    stockcount = int(user.balance[0]['enable_balance'] / (5-len(user.position)) / (buyprice))
-                    # stockcount = int(922281 / 3 / (buyprice * 100))
-                    conn.mystock.trade.insert({"code":item['code'],"buytime":time.strftime("%Y-%m-%d %X", time.localtime()),"buytype":"zrztb","detailtype":"lowopencross0","buyprice":df['price'][0],"tradestatus":0,'stockcount':stockcount})
-                    user.buy(item['code'],float(df['price'][0]),stockcount)
-                    print '账户买入成功'
-                    print '剩余可用资金', user.balance[0]['enable_balance']
-                    print '买入数量', stockcount
+                    #持仓数量小于策略数量时，购买
+                    if(maxstockcount > len(user.position)):
+                        conn.mystock.monitor_weakhardencode.update({'code': item['code'],'status':'lowopen'},{'$set': {'status': 'lowopencross0','isdeal':1}})
+                        # 买入价格高于当前价格，以确定肯定能买到
+                        buyprice = round(float(df['price'][0])*1.02,2)
+                        stockcount = int(user.balance[0]['enable_balance'] / (maxstockcount-len(user.position)) / (buyprice))
+                        # stockcount = int(922281 / 3 / (buyprice * 100))
+                        conn.mystock.trade.insert({"code":item['code'],"buytime":time.strftime("%Y-%m-%d %X", time.localtime()),"buytype":"zrztb","detailtype":"lowopencross0","buyprice":df['price'][0],"tradestatus":0,'stockcount':stockcount})
+                        user.buy(item['code'],float(df['price'][0]),stockcount)
+                        print '账户买入成功'
+                        print '剩余可用资金', user.balance[0]['enable_balance']
+                        print '买入数量', stockcount
 
             #高开低走
             if (item['status'] == 'highopen'):
@@ -71,26 +75,27 @@ while 1:
                     print "GDG，推荐买入！！！",item['code']
                     print "买入时间：",time.strftime("%Y-%m-%d %X", time.localtime())
                     print "买入价格", df['price'][0]
-                    conn.mystock.monitor_weakhardencode.update({'code': item['code'],'status':'highopenlow'},{'$set': {'status': 'highopenlowhigh','isdeal':1}})
 
-                    #买入价格高于当前价格，以确定肯定能买到
-                    buyprice = round(float(df['price'][0]) * 1.02, 2)
-                    #购买股票数量
-                    stockcount = int(user.balance[0]['enable_balance'] / (5-len(user.position)) / (buyprice))
+                    # 持仓数量小于策略数量时，购买
+                    if (maxstockcount > len(user.position)):
+                        conn.mystock.monitor_weakhardencode.update({'code': item['code'], 'status': 'highopenlow'},{'$set': {'status': 'highopenlowhigh', 'isdeal': 1}})
+                        #买入价格高于当前价格，以确定肯定能买到
+                        buyprice = round(float(df['price'][0]) * 1.02, 2)
+                        #购买股票数量
+                        stockcount = int(user.balance[0]['enable_balance'] / (maxstockcount-len(user.position)) / (buyprice))
 
+                        if stockcount<= 0:
+                            print '资金不足'
+                            continue
 
-                    if stockcount<= 0:
-                        print '资金不足'
-                        continue
-
-                    conn.mystock.trade.insert(
-                        {"code": item['code'], "buytime": time.strftime("%Y-%m-%d %X", time.localtime()),
-                         "buytype": "zrztb", "detailtype": "lowopencross0", "buyprice": df['price'][0],
-                         "tradestatus": 0, 'stockcount': stockcount})
-                    user.buy(item['code'], float(df['price'][0]), stockcount)
-                    print '账户买入成功'
-                    print '剩余可用资金', user.balance[0]['enable_balance']
-                    print '买入数量', stockcount
+                        conn.mystock.trade.insert(
+                            {"code": item['code'], "buytime": time.strftime("%Y-%m-%d %X", time.localtime()),
+                             "buytype": "zrztb", "detailtype": "lowopencross0", "buyprice": df['price'][0],
+                             "tradestatus": 0, 'stockcount': stockcount})
+                        user.buy(item['code'], float(df['price'][0]), stockcount)
+                        print '账户买入成功'
+                        print '剩余可用资金', user.balance[0]['enable_balance']
+                        print '买入数量', stockcount
 
 
         except Exception as e:
