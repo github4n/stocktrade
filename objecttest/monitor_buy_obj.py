fcb7fdaad534a167f10f2f6fe72f39f2bc643026
tuoxie119 <516024151@qq.com>
2016-09-19 11:12:27 +0800

    新增交易时增加持股天数

--- objecttest/monitor_buy_obj.py
+++ objecttest/monitor_buy_obj.py
@@ -118,7 +118,7 @@ class buyMonitor:
                                            "buytype": "zrztb", "detailtype": type,
                                            "buyprice": df['price'][0],
                                            "tradestatus": 0, 'stockcount': count, 'maxprice': df['price'][0],
-                                           'buyret': buyret,'lossprice':df['price'][0],'name':df['name'][0]})
+                                           'buyret': buyret,'lossprice':df['price'][0],'name':df['name'][0],'holddays':0})
     #监控器
     def monitor(self):
         while 1:
