9869b1f0a3db0458a0b00eca008956137b581841
tuoxie119 <516024151@qq.com>
2016-09-26 08:59:21 +0800

    去掉止损1/3卖出策略

--- objecttest/monitor_profit.py
+++ objecttest/monitor_profit.py
@@ -9,8 +9,8 @@ class monitor:
     def monitortrade(self):
         conn = pymongo.MongoClient('192.168.222.188', port=27017)
         # for item in conn.mystock.trade.find({'buytime':re.compile('2016-05-27')}):
-        for item in conn.mystock.yjbtrade.find({'tradestatus':0}).sort('buytime',pymongo.ASCENDING):
-        # for item in conn.mystock.yjbtrade.find().sort('buytime', pymongo.DESCENDING).limit(10):
+        # for item in conn.mystock.yjbtrade.find({'tradestatus':0}).sort('buytime',pymongo.ASCENDING):
+        for item in conn.mystock.yjbtrade.find().sort('buytime', pymongo.DESCENDING):
 
             # print item['buytime']
             df = ts.get_realtime_quotes(item['code'])
@@ -30,15 +30,7 @@ class monitor:
 
             # if profit < 10:
             #     continue
-            if(status=='lowopencross0'):
-
-                print '[lowopencross0] ',item['code'],item['name'],item['buytime'],'buy price ',item['buyprice'],'and now price ',nowprice ,'最大收益',round(maxprofit,2),'%','当前收益:',round(nowprofit,2),'%','总收益:',round(profit,2),'%','持股状态:',item['tradestatus']
-            if (status == 'highopenlowhigh'):
-
-                print '[highopenlowhigh] ',item['code'],item['name'],item['buytime'],' buy price ', item['buyprice'],'and now price ', nowprice,'最大收益',round(maxprofit,2),'%','当前收益:',round(nowprofit,2),'%','总收益:',round(profit,2),'%','持股状态:',item['tradestatus']
-
-            if (status == 'predeal'):
-                print '[highopenlowhigh] ', item['code'], item['name'], item['buytime'], ' buy price ', item['buyprice'], 'and now price ', nowprice, '最大收益', round(maxprofit, 2), '%', '当前收益:', round(nowprofit,2), '%', '总收益:', round(profit, 2), '%', '持股状态:', item['tradestatus']
+                print '[',status,'] ', item['code'], item['name'], item['buytime'], ' buy price ', item['buyprice'], 'and now price ', nowprice, '最大收益', round(maxprofit, 2), '%', '当前收益:', round(nowprofit,2), '%', '总收益:', round(profit, 2), '%', '持股状态:', item['tradestatus']
 
 
 monitor().monitortrade()
