57fc6f30decf562e2d9fbe9cae9ed7c1923630ec
tuoxie119 <516024151@qq.com>
2016-09-26 10:01:02 +0800

    去掉止损1/3卖出策略

--- objecttest/monitor_sell_obj.py
+++ objecttest/monitor_sell_obj.py
@@ -60,7 +60,7 @@ class sellMonitor:
                 if item['tradestatus']==0:
 
                     #止损卖出
-                    # sellcount = item['stockcount']
+                    sellcount = item['stockcount']
                     # if nowprice < item['lossprice']:
                     #     print "nowprice",nowprice,"   item['lossprice']",item['lossprice']
                     #     self.sellStock(item['code'].encode("utf-8"), sellprice, sellcount, 'zhisun', item['buytime'])
