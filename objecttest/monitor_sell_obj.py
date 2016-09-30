d253a7baf1792c08dbe1683b9ffa4c39ac5ad880
tuoxie119 <516024151@qq.com>
2016-09-30 09:28:30 +0800

    增加高开低走打印

--- objecttest/monitor_sell_obj.py
+++ objecttest/monitor_sell_obj.py
@@ -115,10 +115,9 @@ class sellMonitor:
                 print 'maxprofit - 3 >= profit'
                 return 1
             # 最大收益为负数
-            if maxprofit <= 0:
-                if profit <= 0:
-                    print 'profit <= 0'
-                    return 1
+            if profit <= 0:
+                print 'profit <= 0'
+                return 1
 
         # 暂时停止使用策略
         # #低于最大收益的30%卖出
